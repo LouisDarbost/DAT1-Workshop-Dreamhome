4e457e980637c45f3b13c9229d79d1271cec2acb
LouisDarbost <Louis.darbost@ada.ac.uk>
2025-02-03 16:20:39 +0000

    01 Saved on DOC

--- queries/testing/index.sql
+++ queries/testing/index.sql
@@ -2,5 +2,5 @@
 .mode column
 
 
-SELECT staffNo, fName, lName, salary - (SELECT AVG(salary)  FROM Staff) AS salDiff FROM Staff  WHERE salary > (SELECT AVG(salary) FROM Staff); 
+SELECT propertyNo, street, city, postcode, type, rooms, rent FROM PropertyForRent WHERE StaffNo IN ( SELECT staffNo FROM Staff WHERE branchNo = (SELECT branchNo FROM Branch WHERE street = '163 Main St'));
 
