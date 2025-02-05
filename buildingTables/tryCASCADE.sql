3ad13ba125eabffbdca0b9b9bd562db2cebcf7ab
LouisDarbost <Louis.darbost@ada.ac.uk>
2025-02-05 10:39:35 +0000

    Wrote Comments on Testing the CASCADE and UPDATING

--- buildingTables/tryCASCADE.sql
+++ buildingTables/tryCASCADE.sql
@@ -17,7 +17,8 @@ CREATE TABLE track (
     trackartist INTEGER DEFAULT 1,
     FOREIGN KEY (trackartist) REFERENCES artist(artistid)
         ON DELETE CASCADE -- When artist is deleted, so is the tracks
-        ON UPDATE SET NULL -- when artistid changes, the tarckartist will be null 
+        ON UPDATE SET NULL -- When artistid changes, the trackartist will be null 
+        -- ON UPDATE SET DEFAULT - When artistid changes, the trackartist will be default 
 );
 
 INSERT INTO artist(artistid, artistname) VALUES
@@ -28,4 +29,8 @@ INSERT INTO track(trackid, trackname, trackartist) VALUES
 (11, 'That is Amore', 1),
 (12, 'Christmas Blues', 1),
 (13, 'My Way', 2);
+
+-- DELETE FROM artist WHERE artistid = 1;   , Dean Martin (1) has been deleted and so has his tracks
+-- UPDATE artist SET artistid = 10 WHERE artistid = 1; Dean Martin(1)has his artist id from 1 to 10. Due to the foreign key, there is no reference anymore to the trackartist. The values in Trackartist is NULL now
+
 
